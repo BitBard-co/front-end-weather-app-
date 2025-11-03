9857afb4f13aea5217f8089a1cecb29d113b9131
Shayan <BitBard-co@users.noreply.github.com>
2025-11-03 14:58:11 +0100

    curl deleted again

--- docs/curl-examples.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Deprecated: This script is no longer used.
-# Use the Make targets from a WSL terminal instead:
-#   make run      # run in foreground
-#   make run-bg   # run in background (logs in /tmp/server.log)
-#   make demo     # quick example requests
-#   make stop     # stop background server
-exit 0
